6b06f7abb9808986b35a733873082b3a1b2de186
Administrator <783719477@qq.com>
2017-04-17 19:19:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/zxingdemo/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.administrator.zxingdemo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.edt_text)
+    TextView mEdtText;
+    @BindView(R.id.btn_create)
+    Button mBtnCreate;
+    @BindView(R.id.iv_result)
+    ImageView mIvResult;
+    @BindView(R.id.btn_scanning)
+    Button mBtnScanning;
+    @BindView(R.id.activity_main)
+    LinearLayout mActivityMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    private Bitmap encodeAsBitmap(String str) {
+        Bitmap bitmap = null;
+        BitMatrix result = null;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            result = multiFormatWriter.encode(str, BarcodeFormat.QR_CODE, 200, 200);
+            // 使用 ZXing Android Embedded 要写的代码
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            bitmap = barcodeEncoder.createBitmap(result);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException iae) { // ?
+            return null;
+        }
+
+        // 如果不使用 ZXing Android Embedded 的话，要写的代码
+
+//        int w = result.getWidth();
+//        int h = result.getHeight();
+//        int[] pixels = new int[w * h];
+//        for (int y = 0; y < h; y++) {
+//            int offset = y * w;
+//            for (int x = 0; x < w; x++) {
+//                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+//            }
+//        }
+//        bitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+//        bitmap.setPixels(pixels,0,100,0,0,w,h);
+
+        return bitmap;
+    }
+
+    @OnClick({R.id.btn_create, R.id.btn_scanning})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_create:
+                Bitmap bitmap = encodeAsBitmap(mEdtText.getText().toString());
+                mIvResult.setImageBitmap(bitmap);
+                break;
+            case R.id.btn_scanning:
+                customScan();
+                break;
+        }
+    }
+
+    public void customScan(){
+        new IntentIntegrator(this)
+                .setOrientationLocked(false)
+                .setCaptureActivity(CustomScanActivity.class) // 设置自定义的activity是CustomActivity
+                .initiateScan(); // 初始化扫描
+    }
+
+    @Override
+// 通过 onActivityResult的方法获取 扫描回来的 值
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult != null) {
+            if(intentResult.getContents() == null) {
+                Toast.makeText(this,"内容为空",Toast.LENGTH_LONG).show();
+            } else {
+                // ScanResult 为 获取到的字符串
+                String ScanResult = intentResult.getContents();
+                Toast.makeText(this,"扫描成功"+ScanResult,Toast.LENGTH_LONG).show();
+
+            }
+        } else {
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+    }
+}
